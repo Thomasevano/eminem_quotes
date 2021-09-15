d727adf62d1abe63cfb4d31f6d6ffb118b573abf
Thomasevano <thomas-evano@hotmail.fr>
2021-09-15 22:16:22 +0200

    Add TailwindCS with DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./index.html', './src/*.{ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
