91e835dcc7776308657768f173f5a2758593cfd5
Thomasevano <thomas-evano@hotmail.fr>
2021-09-27 01:08:52 +0200

    Get random quote using supabase sql function

--- src/http.ts
+++ src/http.ts
@@ -1,48 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
-import { definitions } from '../types/supabase'
 
 const supabase = createClient(`${import.meta.env.VITE_SUPABASE_URL}`, `${import.meta.env.VITE_SUPABASE_KEY}`)
 
 async function getRandomQuote() {
   try {
-    const { data, count } = await supabase
-      // .rpc('getRandomId')
-      .from<definitions['quotes']>('quotes')
-      .select(`
-        quote,
-        songs (
-          listen_url,
-          title,
-          albums (
-            cover_url,
-            name
-          )
-        )
-      `, { count: 'exact'})
-
-    let randomQuoteId = getRandomId(0, count!)
-    return data![randomQuoteId]
-    return data
+    const { data } = await supabase
+      .rpc('get_random_quote')
+    return data![0]
   }
   catch(error) {
     console.error(error)
     return error
   };
 }
-
-function getRandomId(min: number, max: number){
-  return Math.floor(Math.random()*(max-min+1)+min)
-}
-// function getRandomId() {
-//   const get = sql`
-//     SELECT quote, listen_url, title, cover_url, name
-//     FROM quotes, songs, albums
-//     WHERE quotes.song_id = songs.id
-//     AND songs.album_id = albums.id
-//     ORDER_BY random()
-//     LIMIT 1
-//   `
-//   console.log(get)
-// }
 
 export default getRandomQuote
--- src/ui.ts
+++ src/ui.ts
@@ -1,24 +1,21 @@
 const eminemQuote = document.getElementById('quote')!
-// import { definitions } from '../types/supabase'
 
 function showRandomQuote(quote: any) {
-  console.log(quote)
-
   eminemQuote.innerHTML = `
     <div class="card bordered">
       <figure>
-        <img src="${quote.songs.albums.cover_url}" alt="album cover>
+        <img src="${quote.cover_url}" alt="album cover>
       </figure> 
       <div class="card-body">
         <h2 class="card-title">
-          <div class="badge mx-2 badge-primary">Morceau: ${quote.songs.title}</div>
-          <div class="badge mx-2 badge-secondary">Album: ${quote.songs.albums.name}</div>
+          <div class="badge mx-2 badge-primary">Morceau: ${quote.title}</div>
+          <div class="badge mx-2 badge-secondary">Album: ${quote.name}</div>
         </h2>
         <blockquote>
           <p>${quote.quote}.</p>
         </blockquote>
         <div class="justify-center card-actions">
-          <a href="${quote.songs.listen_url}" class="btn btn-secondary" target="_blank">Listen song</a>
+          <a href="${quote.listen_url}" class="btn btn-secondary" target="_blank">Listen song</a>
         </div>
       </div>
     </div>
