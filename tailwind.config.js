5eda477ef2885afa70c39df94febb64cb66d15eb
Thomasevano <thomas-evano@hotmail.fr>
2023-03-11 02:40:15 +0100

    :bug: fix: typescript error while building the app and upgrade tailwindcss config to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./index.html', './src/*.{ts}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/*.ts'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [
