85e8c1e56b7296004ad4183b25bdfc43bf18b67f
Thomasevano <thomas-evano@hotmail.fr>
2026-08-22 17:55:50 +0200

    chore: type and document PocketBase env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_POCKETBASE_URL: string
+  readonly VITE_POCKETBASE_COLLECTION: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
